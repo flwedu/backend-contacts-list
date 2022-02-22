f0c4b8ce40a7f58c564cdffac858d80e44bd8ed8
Eduardo Aquino <edutraquino@gmail.com>
2022-02-22 18:11:03 -0300

    create  repository interface

---
+++ src/application/repositories/IRepository.ts
@@ -0,0 +1,9 @@
+import { Props } from "../../domain/entities/Entity";
+
+export default interface IRepository<T> {
+
+    findById(id: string): Promise<T>;
+    findAll(query: any): Promise<T[]>;
+    save(props: Props<T>, id?: string): Promise<T>;
+    delete(id?: string): Promise<void>;
+}
