23e266951be9c079b36df2e1048f1adc60037a75
Eduardo Aquino <edutraquino@gmail.com>
2022-02-22 19:23:43 -0300

    create use case

---
+++ src/application/use-cases/delete-contact.ts
@@ -0,0 +1,10 @@
+import { Contact } from "../../domain/entities/contact";
+import IRepository from "../repositories/IRepository";
+
+export default class DeleteContact {
+    constructor(private readonly repository: IRepository<Contact>) { }
+
+    async execute(id?: string): Promise<void> {
+        return this.repository.delete(id);
+    }
+}
