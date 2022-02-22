bad760c4d53598ec794cce4128dca9c143e442e6
Eduardo Aquino <edutraquino@gmail.com>
2022-02-22 20:28:01 -0300

    throw error

--- src/application/repositories/test/InMemory-ContactRepository.ts
+++ src/application/repositories/test/InMemory-ContactRepository.ts
@@ -1,5 +1,6 @@
 import IRepository from "../IRepository";
 import { Contact, ContactProps, ContactQueryProps, } from "../../../domain/entities/contact"
+import { ResourceNotFound } from "../../../domain/errors/error";
 
 export default class InMemoryContactRepository implements IRepository<Contact> {
 
@@ -8,7 +9,7 @@ export default class InMemoryContactRepository implements IRepository<Contact> {
     findById(id: string): Promise<Contact> {
         const result = this.list.filter(contact => contact.id === id);
         if (!result.length) {
-            return Promise.reject("Contact not found")
+            throw new ResourceNotFound(`Contact ${id} not found`);
         }
         return Promise.resolve(result[0]);
     }
@@ -28,12 +29,12 @@ export default class InMemoryContactRepository implements IRepository<Contact> {
         if (previousLength > this.list.length) {
             return Promise.resolve();
         }
-        return Promise.reject();
+        throw new ResourceNotFound(`Contact ${id} not found`);
     }
     update(props: ContactProps, id: string): Promise<Contact> {
         const contactIndex = this.list.findIndex(element => element.id === id)
         if (contactIndex === -1) {
-            return Promise.reject("Contact not found");
+            throw new ResourceNotFound(`Contact ${id} not found`);
         }
         const contact = Contact.of(props, id);
         this.list[contactIndex] = contact;
