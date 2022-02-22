ff23c187009879385b668f69a958bbaa075c96ba
Eduardo Aquino <edutraquino@gmail.com>
2022-02-22 19:45:20 -0300

    add tests

---
+++ src/application/use-cases/update-contact.spec.ts
@@ -0,0 +1,36 @@
+import InMemoryContactRepository from "../repositories/test/InMemory-ContactRepository";
+import CreateContact from "./create-contact";
+import UpdateContact from "./update-contact";
+
+describe("update contacts use cases", () => {
+
+    it("should update a contact with a new contact", async () => {
+
+        expect.assertions(3);
+
+        const repository = new InMemoryContactRepository();
+        const createContact = new CreateContact(repository);
+        const sut = new UpdateContact(repository);
+        const spy = jest.spyOn(repository, "update");
+
+        const contact = await createContact.execute({
+            name: "Test",
+            email: "test@test.com",
+            telephone: "(74)0000-0000",
+            imageUrl: "",
+        })
+
+        const updated = await sut.execute({
+            name: "Contact",
+            email: "test@test.com",
+            telephone: "(74)0000-0000",
+            imageUrl: "",
+        }, contact.id)
+
+        //@ts-ignore
+        expect(updated.props.name).toEqual("Contact")
+        //@ts-ignore
+        expect(repository.list[0].props.name).toEqual("Contact");
+        expect(spy).toBeCalledTimes(1);
+    })
+})
