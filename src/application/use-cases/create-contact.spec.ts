982cc671d48fb9d2781d1056cded2bde18dee312
Eduardo Aquino <edutraquino@gmail.com>
2022-02-22 18:43:16 -0300

    create test file

---
+++ src/application/use-cases/create-contact.spec.ts
@@ -0,0 +1,29 @@
+import InMemoryContactRepository from "../repositories/test/InMemory-ContactRepository";
+import CreateContact from "./create-contact";
+
+describe("Create contact use case", () => {
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    })
+
+    it("Should create a new contact", async () => {
+
+        expect.assertions(3);
+
+        const repository = new InMemoryContactRepository();
+        const sut = new CreateContact(repository);
+        const spy = jest.spyOn(repository, "save");
+
+        const contact = await sut.execute({
+            name: "Test",
+            email: "test@test.com",
+            telephone: "(74)0000-0000",
+            imageUrl: "",
+        })
+
+        expect(repository.list.length).toEqual(1);
+        expect(contact).toEqual(repository.list[0]);
+        expect(spy).toBeCalledTimes(1);
+    })
+})
