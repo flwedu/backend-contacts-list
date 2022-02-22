de6363039e832d9c3bc585c3bfe9e0b6912f484d
Eduardo Aquino <edutraquino@gmail.com>
2022-02-22 18:49:09 -0300

    set query param to optional

--- src/application/repositories/IRepository.ts
+++ src/application/repositories/IRepository.ts
@@ -3,7 +3,7 @@ import { Props } from "../../domain/entities/Entity";
 export default interface IRepository<T> {
 
     findById(id: string): Promise<T>;
-    findAll(query: any): Promise<T[]>;
+    findAll(query?: any): Promise<T[]>;
     save(props: Props<T>, id?: string): Promise<T>;
     delete(id?: string): Promise<void>;
 }
