50caf6e251f9eee77fc314b458f0f9a6cb70ab8f
Eduardo Aquino <edutraquino@gmail.com>
2022-02-22 21:42:37 -0300

    create response contracts

---
+++ src/presentation/contracts/http-response-entity.ts
@@ -0,0 +1,19 @@
+export type HttpResponseEntity<T = any> = {
+    statusCode: number,
+    data: T
+};
+
+export const ok = (data: any): HttpResponseEntity => ({
+    statusCode: 200,
+    data
+})
+
+export const notFound = (error: Error): HttpResponseEntity => ({
+    statusCode: 404,
+    data: error.message
+})
+
+export const serverError = (error: Error): HttpResponseEntity => ({
+    statusCode: 500,
+    data: error.message
+})
