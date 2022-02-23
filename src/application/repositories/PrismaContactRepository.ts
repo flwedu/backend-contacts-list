6b5f7852cbbdd638d0a00a6492c437e37866c047
Eduardo Aquino <edutraquino@gmail.com>
2022-02-23 17:58:39 -0300

    implement repository methods

--- src/application/repositories/PrismaContactRepository.ts
+++ src/application/repositories/PrismaContactRepository.ts
@@ -1,41 +1,34 @@
 import { PrismaClient } from "@prisma/client";
 import { Contact, ContactProps } from "../../domain/entities/contact";
 import { ResourceNotFound } from "../../domain/errors/error";
-import ContactAdapter from "../adapters/contact-adapter";
 import IRepository from "./IRepository";
 
 export default class PrismaContactRepository implements IRepository<Contact>{
 
-    private prisma = new PrismaClient();
+    prisma = new PrismaClient();
 
     async findById(id: string): Promise<Contact> {
-        const contact = await this.prisma.contact.findUnique({
+        return this.prisma.contact.findUnique({
             where: { id }
         })
-        if (!contact) {
-            throw new ResourceNotFound(`Contact ${id} not found`);
-        }
-        return ContactAdapter.toEntity({ ...contact });
 
     }
     async findAll(): Promise<Contact[]> {
-        const list = await this.prisma.contact.findMany();
-        return list.map(ContactAdapter.toEntity);
-    }
-    async save(props: ContactProps, id?: string): Promise<Contact> {
+        return await this.prisma.contact.findMany();
 
-        const contact = await this.prisma.contact.create({
-            data: {
-                id,
-                ...props,
-            }
-        });
-        if (!contact) {
-            throw new ResourceNotFound(`Contact ${id} not found`);
-        }
-        else return ContactAdapter.toEntity({ ...contact });
     }
-    async delete(id?: string): Promise<void> {
+    //@ts-ignore
+    async save(props: ContactProps): Promise<Contact> {
+
+        const contact = Contact.of(props);
+        const createdContact = await this.prisma.contact.create({
+            data: {
+                ...contact
+            },
+        });
+        return createdContact;
+    }
+    async delete(id: string): Promise<void> {
 
         try {
             await this.prisma.contact.delete({
@@ -48,10 +41,9 @@ export default class PrismaContactRepository implements IRepository<Contact>{
     }
     async update(props: ContactProps, id: string): Promise<Contact> {
 
-        const contact = await this.prisma.contact.update({
+        return await this.prisma.contact.update({
             where: { id }, data: props
         })
-        return ContactAdapter.toEntity({ ...contact });
     }
 
 }
