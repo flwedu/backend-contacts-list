f3f5deffd7614c5e37eec86530e35491d74fd2fd
Eduardo Aquino <edutraquino@gmail.com>
2022-02-23 18:49:42 -0300

    controller receives parsed request body

--- src/presentation/controllers/save-contact-controller.ts
+++ src/presentation/controllers/save-contact-controller.ts
@@ -1,23 +1,19 @@
 import CreateContact from "../../application/use-cases/create-contact";
-import { Contact, ContactProps } from "../../domain/entities/contact";
+import { Contact } from "../../domain/entities/contact";
 import { ResourceNotFound } from "../../domain/errors/error";
-import { IProvider } from "../providers/IProvider";
-import { HttpResponseEntity, notFound, serverError } from "../contracts/http-response-entity";
+import { HttpResponseEntity, notFound, ok, serverError } from "../contracts/http-response-entity";
 import IController from "../contracts/IController";
+import { IProvider } from "../providers/IProvider";
 
 export default class SaveContactController implements IController {
 
     constructor(private readonly provider: IProvider<Contact>) { }
 
-    async handle(request: Request): Promise<HttpResponseEntity<any>> {
+    async handle(requestData: any): Promise<HttpResponseEntity<any>> {
 
         try {
-            const props = await request.json() as ContactProps;
-            const data = await new CreateContact(this.provider.getRepository()).execute(props);
-            return {
-                statusCode: 200,
-                data
-            }
+            const data = await new CreateContact(this.provider.getRepository()).execute(requestData);
+            return ok(data);
         } catch (error) {
             if (error instanceof ResourceNotFound) {
                 return notFound(error);
