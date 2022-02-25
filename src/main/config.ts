86db67d052d4059cfad7a355d8a3a4d69ea7c9a6
Eduardo Aquino <edutraquino@gmail.com>
2022-02-25 18:05:14 -0300

    function to create a server

---
+++ src/main/config.ts
@@ -0,0 +1,17 @@
+import cors from "cors";
+import express from "express";
+import createExpressRouter from "../infra/routes/express-router";
+import PrismaContactRepository from "../output/repositories/PrismaContactRepository";
+
+export default function App() {
+
+    const repository = new PrismaContactRepository();
+    const router = createExpressRouter(repository);
+    const server = express();
+
+    server.use(cors());
+    server.use(express.json())
+    server.use(router);
+
+    return server;
+}
